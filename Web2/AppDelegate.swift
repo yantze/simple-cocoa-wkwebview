6025726c2f606e7686394136cd3472c4802e2949
yantze <yantze@126.com>
2018-07-04 11:29:08 +0800

    Initial Commit

---
+++ Web2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Web2
+//
+//  Created by yantze on 2018/7/4.
+//  Copyright © 2018 yantze. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
